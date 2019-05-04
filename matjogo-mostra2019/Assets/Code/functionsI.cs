4f563ea2306e5711e800962604d0d9382857e96d
Igor Lemes <igor.s.lemes@gmail.com>
2019-05-04 17:29:37 -0300

    functions to separate classes - updated

--- matjogo-mostra2019/Assets/Code/functionsI.cs
+++ matjogo-mostra2019/Assets/Code/functionsI.cs
@@ -18,9 +18,9 @@ public class functionsI : MonoBehaviour
     // Quatro quadrantes
     int F1(float x, float y)
     {
-            if(x <= 50)
+            if(x <= 50.0f)
             {
-                if(y <= 50)
+                if(y <= 50.0f)
                 {
                     return 3;
                 }
@@ -31,7 +31,7 @@ public class functionsI : MonoBehaviour
 
             } else
             {
-                if (y <= 50)
+                if (y <= 50.0f)
                 {
                     return 4;
                 }
@@ -56,7 +56,7 @@ public class functionsI : MonoBehaviour
     // funções y = x, y = 2x, y = 1/2x
     int F9(float x, float y)
     {
-        if(y >= 2 * x)
+        if(y >= 2.0f * x)
         {
             return 1;
         }
@@ -64,7 +64,7 @@ public class functionsI : MonoBehaviour
         {
             return 2;
         }
-        else if (y >= x / 2)
+        else if (y >= x / 2.0f)
         {
             return 3;
         }
@@ -77,14 +77,65 @@ public class functionsI : MonoBehaviour
     // funções y = 2x-25
     int F10(float x, float y)
     {
-        if (y >= 2 * x - 25)
+        if (Mathf.Sqrt( Mathf.Pow(x - 26.0f, 2.0f) + Mathf.Pow(y - 72.0f, 2.0f) ) <= 17.0f)
+        {
+            return 3;
+        }
+        else if (Mathf.Sqrt(Mathf.Pow(x - 70.0f, 2.0f) + Mathf.Pow(y - 30.0f, 2.0f)) <= 24.0f)
         {
 
         }
+        else if (y >= (2.0f * x - 25.0f))
+        {
+            return 1;
+        }
         else
         {
+            return 2;
+        }
+    }
+    int F14(float x, float y)
+    {
+        float third = 1.0f / 3.0f;
 
+        float retaF = (-3.0f * x) + 200.0f;
+        float retaG = (3.0f * x) - 100.0f;
+        float retaH = ((-1.0f * third) * x) + (200.0f * third);
+        float retaI = (third * x) + (100.0f * third);
+        
+        if (y >= retaI && y < retaH)
+        {
+            return 1;
+        }
+        else if (y >= retaH && y < retaF)
+        {
+            return 4;         
+        }
+        else if (y >= retaF && y >= retaG )
+        {
+            return 2;
+        }
+        else if (y < retaG && y >= retaI)
+        {
+            return 3;
+        }
+        else if (y < retaI && y >= retaH)
+        {
+            return 4;
+        }
+        else if (y < retaH && y >= retaF)
+        {
+            return 1;
+        }
+        else if (y < retaF && y < retaG)
+        {
+            return 3;
+        }
+        else if (y >= retaG && y < retaI)
+        {
+            return 2;
         }
     }
 
 }
+
