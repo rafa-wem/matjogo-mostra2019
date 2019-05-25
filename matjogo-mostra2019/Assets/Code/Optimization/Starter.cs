16ba119d91fb4ea05a3d39c3410235817b087bd8
LeafarTHENERD <leafar.thenerd@hotmail.com>
2019-05-25 05:40:02 -0300

    Fix loop on empty scene selected

--- matjogo-mostra2019/Assets/Code/Optimization/Starter.cs
+++ matjogo-mostra2019/Assets/Code/Optimization/Starter.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Starter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(Random.Range(0, SceneManager.sceneCount));
+        SceneManager.LoadScene(Random.Range(1, SceneManager.sceneCount));
     }
 
     // Update is called once per frame
